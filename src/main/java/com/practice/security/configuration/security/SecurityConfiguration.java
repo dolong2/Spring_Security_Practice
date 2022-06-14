37ee82dd38470296ab28761f0d6333f8a4583dd9
jeayoung <k01066624566@gmail.com>
2022-06-14 09:16:51 +0900

    add FilterChain

--- src/main/java/com/practice/security/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/practice/security/configuration/security/SecurityConfiguration.java
@@ -1,20 +1,26 @@
 package com.practice.security.configuration.security;
 
+import com.practice.security.configuration.security.handler.CustomAccessDeniedHandler;
+import com.practice.security.configuration.security.handler.CustomAuthenticationEntryPointHandler;
 import com.practice.security.configuration.security.jwt.JwtRequestFilter;
 import com.practice.security.configuration.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration{
-    private final TokenProvider tokenProvider;
+
     private final JwtRequestFilter jwtRequestFilter;
 
     @Bean
@@ -28,6 +34,29 @@ public class SecurityConfiguration{
                 .and()
                 .ignoring()
                 .antMatchers("/h2-console/**/**");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        System.out.println("jwtRequestFilter = " + jwtRequestFilter);
+        http
+                .cors()
+                .and()
+                .csrf().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                    .antMatchers("/v1/admin/**").hasRole("ADMIN")
+                    .antMatchers("/v1/member/**").hasRole("MEMBER")
+                    .anyRequest().permitAll()
+                .and()
+                .exceptionHandling()
+                    .accessDeniedHandler(new CustomAccessDeniedHandler())
+                    .authenticationEntryPoint(new CustomAuthenticationEntryPointHandler())
+                .and()
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 
     @Bean
