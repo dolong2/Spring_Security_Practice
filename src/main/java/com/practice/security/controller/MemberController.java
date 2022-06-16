8b8fd90a325a63a1b4241af70d7cfe145c505232
jeayoung <k01066624566@gmail.com>
2022-06-16 13:53:27 +0900

    add MemberController

--- src/main/java/com/practice/security/controller/MemberController.java
+++ src/main/java/com/practice/security/controller/MemberController.java
@@ -1,11 +1,52 @@
 package com.practice.security.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.practice.security.dto.request.MemberReqDto;
+import com.practice.security.dto.request.SignInDto;
+import com.practice.security.service.MemberService;
+import com.practice.security.util.response.ResponseService;
+import com.practice.security.util.response.result.CommonResultResponse;
+import com.practice.security.util.response.result.ListResultResponse;
+import com.practice.security.util.response.result.SingleResultResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/v1/member")
 public class MemberController {
+    private final MemberService memberService;
+    private final ResponseService responseService;
 
+    @PostMapping("/join")
+    public CommonResultResponse join(@RequestBody MemberReqDto memberReqDto){
+        memberService.join(memberReqDto);
+        return responseService.getSuccessResult();
+    }
+
+    @PostMapping("/login")
+    public SingleResultResponse login(@RequestBody SignInDto signInDto){
+        return responseService.getSingleResult(memberService.login(signInDto));
+    }
+
+    @PostMapping("/logout")
+    public CommonResultResponse logout(){
+        memberService.logout();
+        return responseService.getSuccessResult();
+    }
+
+    @GetMapping("/{memberIdx}")
+    public CommonResultResponse getOneMember(@PathVariable Long memberIdx){
+        return responseService.getSingleResult(memberService.getMemberByIdx(memberIdx));
+    }
+
+    @DeleteMapping
+    public CommonResultResponse withdrawal(){
+        memberService.withdrawal();
+        return responseService.getSuccessResult();
+    }
+
+    @GetMapping
+    public ListResultResponse getAllMember(){
+        return responseService.getListResult(memberService.getAllMember());
+    }
 }
